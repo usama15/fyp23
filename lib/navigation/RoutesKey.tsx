9438fbc197497c3ee6fe477ccd953054dc9272fe
usama shakeel <princeusamaansari15@gmail.com>
2023-01-29 19:50:49 +0500

    update

--- lib/navigation/RoutesKey.tsx
+++ lib/navigation/RoutesKey.tsx
@@ -5,7 +5,11 @@ class RoutesKey {
     static SIGNUP = 'Signup'; 
     static STUDENTBOTTOM = 'Student Bottom'; 
     static ACADEMICBOTTOM = 'Academic Bottom'; 
+    static FINANCEBOTTOM = 'Finance Bottom'; 
+    static ADMISSIONBOTTOM = 'Admission Bottom'; 
     static APPLICATIONDETAIL = 'Application Detail';
+    static FINANCAPPLICATIONDETAIL = 'Finance Application Detail';
+    static ADMISSIONAPPLICATIONDETAIL = 'Admission Application Detail';
 }
 
 export default RoutesKey;
